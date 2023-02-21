14204d1e1068ef524ff198a7a47180551a272143
cat-to <125943734+cat-to@users.noreply.github.com>
2023-02-21 12:09:29 +0800

    Add files via upload

---
+++ login form.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import './LoginForm.css';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // TODO: Submit the form data to the server for authentication
+    console.log(`Username: ${username}, Password: ${password}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="login-form">
+      <h2>Login Form</h2>
+      <div className="input-field">
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+      </div>
+      <div className="input-field">
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </div>
+      <button type="submit">Log In</button>
+    </form>
+  );
+};
+
+export default LoginForm;
